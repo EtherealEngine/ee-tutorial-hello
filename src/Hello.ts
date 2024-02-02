fc6e742c9a4869e42b3f54643ecfb003fc55918d
Ivan Mar (sOkam!) <7308253+heysokam@users.noreply.github.com>
2024-02-01 18:38:55 -0800

    fix: Update imports for the spatial refactor

--- src/Hello.ts
+++ src/Hello.ts
@@ -1,24 +1,23 @@
 import { ECS } from '@etherealengine/ecs'
-import { PhysicsSystem } from '@etherealengine/engine/src/physics/PhysicsModule'
-import { TransformComponent } from '@etherealengine/engine/src/transform/components/TransformComponent'
+import { PhysicsSystem } from '@etherealengine/spatial/src/physics/PhysicsModule'
+import { TransformComponent } from '@etherealengine/spatial/src/transform/components/TransformComponent'
 import { PrimitiveGeometryComponent } from '@etherealengine/engine/src/scene/components/PrimitiveGeometryComponent'
-import { createSceneEntity } from '@etherealengine/engine/src/ecs/functions/createSceneEntity'
 
 let initialized = false
 const hello = () => {
   if(initialized) return
   initialized = true
 
-  const entity = createSceneEntity('hello-world', null,)
+  const entity = ECS.createSceneEntity('hello-world', null,)
   ECS.setComponent(entity, PrimitiveGeometryComponent, { geometryType: 1 })
-  ECS.getComponent(entity,TransformComponent).position.set(x,y,z)
+  ECS.getComponent(entity, TransformComponent).position.set(0,1,0)
 }
 
-export default async function worldInjection() {
-  export const HelloWorldSystem = ECS.defineSystem({
-    uuid: 'hellworld.system',
-    execute: hello,
-    insert: { after: PhysicsSystem }
-  })
-}
+export const HelloWorldSystem = ECS.defineSystem({
+  uuid: 'hellworld.system',
+  execute: hello,
+  insert: { after: PhysicsSystem }
+})
+
+export default async function worldInjection() {}
 
